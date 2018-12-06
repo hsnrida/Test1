3853cb852221ec600f3f12ece0356d4351e5fbd0
hsnrida <hasan_rida@hotmail.com>
2018-12-06 09:20:21 +0200

    added hungry prompt

--- Source.c
+++ Source.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 void main() {
-	printf("I'm Hungry");
+	char hungry[20];
+	printf("are you hungry?\n");
+	scanf_s("%s", hungry, sizeof(hungry));
+	if (strcmp(hungry,"yes")==0) {
+		printf("eat sousa\n");
+	}
+	else
+		printf("do your HW\n");
 	getchar();
+	system("pause");
 }
